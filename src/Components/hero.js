b238f12b702e50419f963eec982645c96044b1cd
AlabiMuktar <109441128+AlabiMuktar@users.noreply.github.com>
2024-01-04 01:54:06 +0100

    setting up route

---
+++ src/Components/hero.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { AiOutlineMenu } from "react-icons/ai";
+import { IoCartOutline } from "react-icons/io5";
+
+const Hero = () => {
+  return (
+    <div className='px-10 lg:px-[6rem]'>
+        <div className='flex justify-between text-sm text-[#949494]'>
+            <ul className='flex gap-x-2 cursor-pointer'>
+                <li className='flex items-center gap-x-2 p-2 text-black hover:p-2 hover:bg-[#F68B1E]'><AiOutlineMenu />Shop by categories</li>
+                <li className='ml-4 p-2'>Todays Deal</li>
+                <li className='p-2'>Special Prices</li>
+                <li className='p-2'>Favorite</li>
+                <li className='p-2'>Recently viewed</li>
+            </ul>
+            <ul className='cursor-pointer'>
+                <li className='flex items-center gap-x-2 text-black'>Cart<IoCartOutline /></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/Pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Components/navbar'
+import Hero from '../Components/hero'
+
+function Home() {
+  return (
+    <div>
+        <Navbar />
+        <Hero />
+    </div>
+  )
+}
+
+export default Home
