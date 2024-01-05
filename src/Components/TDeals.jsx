5d2341f55f0611710a78af5ccd1d2646a2d493b0
prex12 <114931845+prex12@users.noreply.github.com>
2024-01-05 13:17:18 +0100

    done with the todays deal

--- src/Components/TDeals.jsx
+++ src/Components/TDeals.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import ShopItem from './shopitem'
 
 function TDeals() {
   return (
-    <div className='lg:mt-4 lg:px-[6rem]'>
-        <h3>Today's Deals </h3>
-        <Link to='TodaysDeals'></Link>
-    </div>
+    <>
+        <div className='flex justify-between align-middle lg:mt-4 lg:px-[6rem]'>
+            <h3>Today's Deals </h3>
+            <Link to='TodaysDeals' className='underline text-orange-500'>see all</Link>
+        </div>
+        <div  className='lg:mt-4 lg:px-[6rem]'>
+            <ShopItem />
+        </div>
+    </>
+    
   )
 }
 
--- src/Components/shopitem.jsx
+++ src/Components/shopitem.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { FaRegHeart } from "react-icons/fa6";
 
 function ShopItem() {
   return (
-    <div className='flex flex-col w-[250px] h-[150px] bg-green-300'>
+    <div className='flex flex-col gap-4 w-[14rem] '>
         <div className='flex justify-between align-middle'>
-          <p className='text-sm bg-orange-300 text-orange-500'>20% Off</p>
-          <p className='text-sm'><FaRegHeart /></p>
+          <p className='text-[.8rem] px-1 bg-orange-200 text-orange-500'>20% Off</p>
+          <p><FaRegHeart /></p>
+        </div>
+        <div className='flex flex-col gap-4 w-full'>
+          <img src={require("../Assets/Images/phone.png")} alt="product img" />
+          <div className='flex align-middle justify-between px-1'>
+            <p className='text-base font-semibold'>Infinix Hot 30i</p>
+            <p className='text-base font-semibold'>#90,000</p>
+          </div>
+          <p className='px-1 text-[1rem] font-normal text-[#949494]'>6.6'" HD+, 4+4GB RAM /128GB ROM Android 12 - Black</p>
+          <button className='h-[2.5rem]  bg-[#F68B1E]'>Buy now</button>
         </div>
     </div>
   )
